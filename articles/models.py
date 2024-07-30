560a1373390662fff623de8988d811ccfc067990
EHYONGI <irene4618@naver.com>
2024-07-30 10:43:11 +0900

    모델링

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) # 시간정보 자동 등록(글 쓰기 할 때 '어제 등록했어' X처럼)
