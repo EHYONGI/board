c4885d931a1cef7398f61ce696651f6466a8f24c
EHYONGI <irene4618@naver.com>
2024-07-30 12:02:37 +0900

    Read(1) 기능 구현

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="index"),
+    path('<int:id>/', views.detail, name="detail") # posts(articles)/ 필요 X -> 중복된 거 빼서 !
 ]
