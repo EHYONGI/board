1694f3d9d70e3e60e023bcfd42eb1e2ebf1be7cb
EHYONGI <irene4618@naver.com>
2024-07-30 14:08:03 +0900

    Create, Delete 기능 구현

--- articles/urls.py
+++ articles/urls.py
@@ -5,5 +5,8 @@ app_name = 'articles'
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('<int:id>/', views.detail, name="detail") # posts(articles)/ 필요 X -> 중복된 거 빼서 !
+    path('<int:id>/', views.detail, name="detail"), # posts(articles)/ 필요 X -> 중복된 거 빼서 !
+    path('new/', views.new, name="new"),
+    path('create/', views.create, name='create'),
+    path('<int:id>/delete/', views.delete, name='delete'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
 
@@ -20,3 +20,23 @@ def detail(request, id):
     }
 
     return render(request, 'detail.html', context)
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    article = Article()
+    article.title = title
+    article.content = content
+    article.save()
+
+    return redirect('articles:detail', id=article.id) # = '/articles/<id>/'
+
+def delete(request, id):
+    article = Article.objects.get(id=id) # 아티클 찾고 변수에 저장
+    article.delete()
+
+    return redirect('articles:index')
